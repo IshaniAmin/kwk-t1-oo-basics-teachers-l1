13d5c9aada3d4ea392d62cfc3827ddf764311190
Ishani <ishaniamin@gmail.com>
2018-06-14 19:46:34 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -42,13 +42,10 @@ end
 
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
   def initialize(title)
     @title = title
-  end
-
-  def title
-    @title
   end
 
   def turn_page
